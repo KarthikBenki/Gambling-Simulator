f129ff91b32a92bd25f0db81fe0f937f4e9ad139
karthikbenki <karthikmc007@gmail.com>
2022-02-09 12:05:45 +0530

    Added: stake and bet initialization logic

--- src/com/bridgelabz/demo/GamblingSimulation.java
+++ src/com/bridgelabz/demo/GamblingSimulation.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.demo;
 
 public class GamblingSimulation {
+	static final int STAKE_PER_DAY = 100;
+	static final int BET_PER_GAME = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulation");
