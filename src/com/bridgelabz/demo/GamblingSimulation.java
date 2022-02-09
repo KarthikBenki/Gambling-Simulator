a35653b0e35334c42cd843f7a831747f1df31f81
karthikbenki <karthikmc007@gmail.com>
2022-02-09 12:57:50 +0530

    Added:Place a bet to ckeck win or loss

--- src/com/bridgelabz/demo/GamblingSimulation.java
+++ src/com/bridgelabz/demo/GamblingSimulation.java
@@ -5,11 +5,10 @@ import java.util.Random;
 public class GamblingSimulation {
 	static final int STAKE_PER_DAY = 100;
 	static final int BET_PER_GAME = 1;
+	static final int WIN = 1;
+	static int stake = 1;/// intial gambler bets with 1
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Gambling Simulation");
-		int stake = 1;/// intial gambler bets with 1
-		final int WIN = 1;
+	public static void winCheck() {
 		Random random = new Random();
 		int betReturns = random.nextInt(2);
 		if (betReturns == WIN) {
@@ -21,6 +20,11 @@ public class GamblingSimulation {
 		}
 		System.out.println("stake = " + stake);
 
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulation");
+		winCheck();// calling function
 	}
 
 }
